f406791cc1f894d97fefa555ff9cea197f802717
PaoloSalazar <paolo.salazar@audio.com>
2026-01-19 08:02:32 -0400

    MBRA-5 Error at moment of install psycopg2-binary

--- data/init.py
+++ data/init.py
@@ -1,6 +1,5 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import pg8000
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,7 +12,8 @@ def get_db():
     if conn:
         return
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/musical_band_db")
-    conn = psycopg2.connect(DATABASE_URL)
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
+    conn = pg8000.connect(DATABASE_URL)
+    cursor = conn.cursor()
+    cursor.row_factory = lambda cursor, row: dict(zip([col[0] for col in cursor.description], row))
 
 get_db()
