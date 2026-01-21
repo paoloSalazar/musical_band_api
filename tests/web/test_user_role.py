e9c096271e08fe4e2684ac265feac6a169caeedb
PaoloSalazar <paolo.salazar@audio.com>
2026-01-21 07:22:40 -0400

    Added and fixed user-role CRUD endpoints

--- tests/web/test_user_role.py
+++ tests/web/test_user_role.py
@@ -1,10 +1,9 @@
-# from fastapi.testclient import TestClient
-# from main import app
+from fastapi.testclient import TestClient
+from main import app
 
-# client = TestClient(app)
+client = TestClient(app)
 
 def test_get_user_roles():
-    # response = client.get("/api/user-roles/")
-    # assert response.status_code == 200
-    # assert isinstance(response.json(), list)
-    pass
+    response = client.get("/api/user-roles/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
